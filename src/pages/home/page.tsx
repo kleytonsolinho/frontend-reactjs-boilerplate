561bfc1ae801d2c44d03be3ce6c57d80a55276a2
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 15:58:16 -0300

    feat: Add Home page component

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Você deve adicionar nesse component a renderização do seu projeto.
+ *
+ */
+
+import { TemplateBoilerplate } from './components/template-boilerplate'
+
+export function Home() {
+	return <TemplateBoilerplate />
+}
