285e284c3352e95e698810e8e71a8cb757b12b08
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 11:37:01 -0300

    fix: Refactor app.tsx component

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,12 @@
-/** 
+/**
  * Você deve adicionar nesse component a renderização do seu projeto.
- * 
-*/
+ *
+ */
 
-
-import { TemplateBoilerplate } from "./template-boilerplate"
+import { TemplateBoilerplate } from './template-boilerplate'
 
 function App() {
-  
-  return (
-    <TemplateBoilerplate />
-  )
+	return <TemplateBoilerplate />
 }
 
 export default App
