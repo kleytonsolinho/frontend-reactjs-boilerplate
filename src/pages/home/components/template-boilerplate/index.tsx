eb741ce46ff5e981df73acbe605a78ffd3a63357
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 19:31:28 -0300

    fix: Refactor template-boilerplate component

--- src/pages/home/components/template-boilerplate/index.tsx
+++ src/pages/home/components/template-boilerplate/index.tsx
@@ -5,13 +5,10 @@
  */
 
 import reactLogo from '@/assets/svg/react.svg'
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { CountBoilerplate } from '@/components/count-boilerplate'
 import viteLogo from '/vite.svg'
 
 export function TemplateBoilerplate() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center justify-center gap-y-8">
 			<h1 className="text-4xl">BexUp Frontend Boilerplate</h1>
@@ -28,19 +25,7 @@ export function TemplateBoilerplate() {
 				</a>
 			</div>
 			<h2 className="text-2xl">Vite + React</h2>
-			<div className="flex w-[400px] flex-col items-center rounded-md border px-4 py-8">
-				<Button
-					className="rounded-md border bg-slate-100 px-4 py-2 text-black hover:text-white"
-					onClick={() => setCount((count) => count + 1)}
-				>
-					count is {count}
-				</Button>
-				<div className="mt-4 flex flex-col text-center">
-					<span>Edit</span>
-					<code>src/template-boilerplate.tsx</code>
-					<span>and save to test HMR</span>
-				</div>
-			</div>
+			<CountBoilerplate />
 			<p className="read-the-docs">
 				Leia a documentação do boilerplate para mais informações clicando{' '}
 				<a
