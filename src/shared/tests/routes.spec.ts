1ab7e1a70eaa46f646a772dc159685a6036c72d7
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 17:59:46 -0300

    test: Remove routes.spec.ts test file

--- src/shared/tests/routes.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { routes } from '../routes'
-
-describe('Routes', () => {
-	it('should have a HOME route', () => {
-		expect(routes).toHaveProperty('HOME')
-		expect(routes.HOME).toBe('/')
-	})
-})
