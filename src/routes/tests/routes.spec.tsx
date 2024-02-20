607b5cb0d2518fece4d29c074770b24cb96fde90
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 15:58:43 -0300

    test: Add unit test for AppRoutes component

---
+++ src/routes/tests/routes.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+
+import { routes } from '@/shared/routes'
+import { BrowserRouter } from 'react-router-dom'
+import { beforeAll, describe, expect, it } from 'vitest'
+import { AppRoutes } from '../routes'
+
+describe('App Routes', () => {
+	beforeAll(async () => {
+		render(
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		)
+	})
+
+	it('should render element on HOME route', async () => {
+		window.history.pushState({}, 'Home page', routes.HOME)
+
+		const button = screen.queryByText('count is 0')
+		expect(button).toBeInTheDocument()
+	})
+})
