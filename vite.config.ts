26bc394faa28c463be9b8540b62fd5d1e7ec39cb
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 10:58:55 -0300

    chore: Add plugins and configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import svgrPlugin from 'vite-plugin-svgr'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), viteCommonjs()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'build',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  server: {
+    open: true,
+    port: 3000,
+    host: true,
+  },
+  define: {
+    'process.env': {},
+  },
 })
