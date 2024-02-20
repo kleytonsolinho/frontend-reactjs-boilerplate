7ecb73263469867fc8e9050fff5f70dc1fe6df64
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 15:57:01 -0300

    feat: Add vitest configuration for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,54 @@
+/// <reference types="vitest" />
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 import svgrPlugin from 'vite-plugin-svgr'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config'
+
+const vitestConfig: VitestUserConfigInterface = {
+	test: {
+		environment: 'jsdom',
+		setupFiles: ['./src/vitest.setup.ts'],
+		include: ['./src/**/*.spec.{js,ts,jsx,tsx}'],
+		exclude: ['node_modules', 'build', 'coverage', 'cypress'],
+		coverage: {
+			exclude: [
+				'src/**.{js,ts,jsx,tsx}',
+				'postcss.config.js',
+				'tailwind.config.js',
+				'.eslintrc.cjs',
+				'src/lib/**',
+				'src/styles/**',
+				'src/types/**',
+				'src/assets/**',
+			],
+		},
+	},
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), viteCommonjs()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'build',
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  server: {
-    open: true,
-    port: 3000,
-    host: true,
-  },
-  define: {
-    'process.env': {},
-  },
+	plugins: [react(), viteTsconfigPaths(), svgrPlugin(), viteCommonjs()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		outDir: 'build',
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+	},
+	test: vitestConfig.test,
+	server: {
+		open: true,
+		port: 3000,
+		host: true,
+	},
+	define: {
+		'process.env': {},
+	},
 })
