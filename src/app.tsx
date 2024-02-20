e395b8db807a6f5935726cab4e5756abe126582c
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-20 10:59:34 -0300

    feat: Add App component to render project

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+/** 
+ * Você deve adicionar nesse component a renderização do seu projeto.
+ * 
+*/
+
+
+import { TemplateBoilerplate } from "./template-boilerplate"
+
+function App() {
+  
+  return (
+    <TemplateBoilerplate />
+  )
+}
+
+export default App
