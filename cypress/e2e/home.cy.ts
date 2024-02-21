e7c7919e81beb995ee1d73d54cf177158314c693
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-21 16:37:09 -0300

    test: Update home.cy.ts with language selection and button count functionality

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,12 +1,35 @@
 describe('Home', () => {
-	it('should render the home page', () => {
+	beforeEach(() => {
 		cy.visit('http://localhost:3000/')
-		cy.get('h1').should('have.text', 'BexUp Frontend Boilerplate')
+	})
+
+	it('should render the home page', () => {
+		cy.get('[data-cy="home-title"]').should(
+			'have.text',
+			'BexUp Frontend Boilerplate'
+		)
 	})
 
 	it('should render counter on click button', () => {
-		cy.visit('http://localhost:3000/')
-		cy.get('button').click()
-		cy.get('button').should('have.text', 'count is 1')
+		cy.get('[data-cy="home-button-count"]').click()
+		cy.get('[data-cy="home-button-count"]').should('have.text', 'count is 1')
+	})
+
+	it('should change language to english', () => {
+		cy.get('[data-cy="language-select-en"]').click()
+		cy.get('[data-cy="home-language-text"]').should('have.text', 'Hello World!')
+	})
+
+	it('should change language to spanish', () => {
+		cy.get('[data-cy="language-select-es"]').click()
+		cy.get('[data-cy="home-language-text"]').should(
+			'have.text',
+			'¡Hola, mundo!'
+		)
+	})
+
+	it('should change language to portuguese', () => {
+		cy.get('[data-cy="language-select-pt"]').click()
+		cy.get('[data-cy="home-language-text"]').should('have.text', 'Olá mundo!')
 	})
 })
