7de41293643fbb2806c83c177c3151797ea9c2f6
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-22 14:56:23 -0300

    feat: Add language resources file

---
+++ src/language/resources.d.ts
@@ -0,0 +1,13 @@
+interface Resources {
+  "pt-BR": {
+    "translation": {
+      "home": {
+        "hello": "Olá mundo!",
+        "readDocs": "Leia a documentação do boilerplate para mais informações clicando",
+        "clickHere": "aqui"
+      }
+    }
+  }
+}
+
+export default Resources;
