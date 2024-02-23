c4d92619337b4246b7a7f8cf08c16b3fd88e4c99
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-23 14:16:49 -0300

    feat: Add TanstackQueryProvider and Toaster component

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import './language/i18n.ts'
 import { AppRoutes } from './routes/routes.tsx'
+import { TanstackQueryProvider } from './services/tanstack-query/provider.tsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<AppRoutes />
-		</BrowserRouter>
+		<TanstackQueryProvider>
+			<BrowserRouter>
+				<AppRoutes />
+				<Toaster richColors position="top-right" />
+			</BrowserRouter>
+		</TanstackQueryProvider>
 	</React.StrictMode>
 )
