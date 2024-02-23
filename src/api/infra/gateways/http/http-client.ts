832c07587cfc49c6a712d3832a26a423e50d958f
Kleyton Solinho <kleytonsolinho@gmai.com>
2024-02-23 14:18:21 -0300

    feat: Add HttpClient class for making HTTP requests

---
+++ src/api/infra/gateways/http/http-client.ts
@@ -0,0 +1,38 @@
+export type HttpMethod = 'get' | 'post' | 'put' | 'delete'
+
+export enum HttpStatusCode {
+	ok = 200,
+	created = 201,
+	noContent = 204,
+	badRequest = 400,
+	unauthorized = 401,
+	forbidden = 403,
+	methodNotAllowed = 405,
+	notFound = 404,
+	serverError = 500,
+}
+
+export interface HttpRequest {
+	url: string
+	method: HttpMethod
+	headers?: any
+	body?: any
+	params?: any
+}
+
+export interface HttpResponse<R = any> {
+	statusCode: HttpStatusCode
+	body?: R
+}
+
+export interface IHttpClient<R = any> {
+	execute: (data: HttpRequest) => Promise<HttpResponse<R>>
+}
+
+export class HttpClient implements IHttpClient {
+	constructor(private readonly client: IHttpClient) {}
+
+	async execute(data: HttpRequest): Promise<HttpResponse> {
+		return this.client.execute(data)
+	}
+}
