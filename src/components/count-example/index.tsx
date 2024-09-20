b359a7138359281410e50bd730fb7d76e958cfd3
kleytonsolinho <kleytonsolinho@gmail.com>
2024-09-20 14:39:25 -0300

    refactor: Add data-testid to home button in CountExample component

--- src/components/count-example/index.tsx
+++ src/components/count-example/index.tsx
@@ -16,6 +16,7 @@ export function CountExample({ count = 0, handleClick }: CountExampleProps) {
 		<div className="flex w-[400px] flex-col items-center rounded-md border px-4 py-8">
 			<Button
 				data-cy="home-button-count"
+				data-testid="home-button-count"
 				className="rounded-md border bg-slate-100 px-4 py-2 text-black hover:text-white"
 				onClick={handleClick}
 			>
