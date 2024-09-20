275a912b4f87df95c819dd63d678ba8ffcfdb454
kleytonsolinho <kleytonsolinho@gmail.com>
2024-09-20 14:39:37 -0300

    refactor: Update i18n configuration in language file

--- src/language/i18n.ts
+++ src/language/i18n.ts
@@ -4,20 +4,25 @@ import { initReactI18next } from 'react-i18next'
 import translationEN from './en/en-US.json'
 import translationES from './es/es-ES.json'
 import translationPT from './pt/pt-BR.json'
+import {
+	DEFAULT_LANGUAGE_I18NEXT,
+	LANGUAGE_I18NEXT_KEY,
+} from '@/shared/constants'
 
-export const DEFAULT_LANGUAGE = 'pt'
+const savedLanguage =
+	localStorage.getItem(LANGUAGE_I18NEXT_KEY) || DEFAULT_LANGUAGE_I18NEXT
 
 i18next
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		debug: process.env.NODE_ENV === 'development',
-		fallbackLng: DEFAULT_LANGUAGE,
-		lng: DEFAULT_LANGUAGE,
+		fallbackLng: DEFAULT_LANGUAGE_I18NEXT,
+		lng: savedLanguage,
 	})
 
 i18next.addResourceBundle('pt', 'translation', translationPT['translation'])
 i18next.addResourceBundle('en', 'translation', translationEN['translation'])
 i18next.addResourceBundle('es', 'translation', translationES['translation'])
 
-  export const i18n = i18next
+export const i18n = i18next
