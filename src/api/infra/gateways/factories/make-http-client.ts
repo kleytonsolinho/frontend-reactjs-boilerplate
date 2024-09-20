24266f207cdf1fc0d738b9487fbdbdf71a85a341
kleytonsolinho <kleytonsolinho@gmail.com>
2024-09-20 14:38:54 -0300

    refactor: Update makeHttpClient to use AxiosHttpClient

--- src/api/infra/gateways/factories/make-http-client.ts
+++ src/api/infra/gateways/factories/make-http-client.ts
@@ -1,6 +1,8 @@
 import { AxiosHttpClient } from '../http/axios/axios-http-client'
 import { IHttpClient } from '../http/http-client'
+// import { FetchHttpClient } from '../http/fetch/fetch-http-client'
 
 export function makeHttpClient(): IHttpClient {
 	return new AxiosHttpClient()
+	// return new FetchHttpClient()
 }
